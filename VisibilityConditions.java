b95d8814eff0cfc1089bbc4e7a548cd9fbfd57eb
Wenfeng Wang <wangwenfeng2017@gmail.com>
2017-07-09 14:55:47 -0400

    Remove main method from VisibilityCondition.java

--- VisibilityConditions.java
+++ VisibilityConditions.java
@@ -10,7 +10,65 @@ public class VisibilityConditions {
 	/**
 	 * @param args
 	 */
+	public static void visibilityTest() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Wang\\Desktop\\Selenium_training\\firefoxDriver\\geckodriver.exe");   
 
+
+		// objects and variables instantiation
+		WebDriver driver = new FirefoxDriver();
+		String appUrl = "https://google.com";
+
+		// launch the firefox browser and open the application url
+		driver.get(appUrl);
+
+		// maximize the browser window
+		driver.manage().window().maximize();
+
+		// declare and initialize the variable to store the expected title of the webpage.
+		String expectedTitle = "Google";
+
+		// fetch the title of the web page and save it into a string variable
+		String actualTitle = driver.getTitle();
+
+		// compare the expected title of the page with the actual title of the page and print the result
+		if (expectedTitle.equals(actualTitle))
+		{
+			System.out.println("Verification Successful - The correct title is displayed on the web page.");
+		}
+		else
+		{
+			System.out.println("Verification Failed - An incorrect title is displayed on the web page.");
+		}
+
+		// verify if the “Google Search” button is displayed and print the result
+		boolean submitbuttonPresence=driver.findElement(By.name("btnK")).isDisplayed();
+		System.out.println(submitbuttonPresence);
+
+		// enter the keyword in the “Google Search” text box by which we would want to make the request
+		WebElement searchTextBox = driver.findElement(By.id("lst-ib"));
+		searchTextBox.clear();
+		searchTextBox.sendKeys("Selenium");
+
+		// verify that the “Search button” is displayed and enabled
+		boolean searchIconPresence = driver.findElement(By.name("btnK")).isDisplayed();
+		boolean searchIconEnabled = driver.findElement(By.name("btnK")).isEnabled();
+
+		if (searchIconPresence==true && searchIconEnabled==true)
+		{
+			// click on the search button
+			WebElement searchIcon = driver.findElement(By.name("btnK"));
+			searchIcon.click();
+		}
+
+		// close the web browser
+		driver.close();
+		System.out.println("Test script executed successfully.");
+
+		// terminate the program
+		System.exit(0);
+	}
+/*		
+	}
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Wang\\Desktop\\Selenium_training\\firefoxDriver\\geckodriver.exe");   
@@ -69,5 +127,6 @@ public class VisibilityConditions {
 		// terminate the program
 		System.exit(0);
 	}
+	*/
 }
 
